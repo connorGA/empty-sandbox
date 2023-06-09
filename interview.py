dccfe265bdbf64c172f11c20274b6033d080b842
connorGA <ronnoccap@gmail.com>
2023-06-08 21:01:06 -0700

    not correct yet

---
+++ interview.py
@@ -0,0 +1,18 @@
+alarms = [{"Alarm 1", "Alarmo", "Red"},
+ {"Alarm 2", "Alarma", "Blue"},
+ {"Alarm 3", "Alarmee", "Blue"},
+ {"Alarm 4", "Alarmy", "Red"},
+ {"Alarm 5", "Alarmish", "Blue"},]
+
+# Write a function that iterates through the data and counts the alarms that are Blue
+
+def countAlarms(alarms):
+    count = 0
+
+    for alarm in alarms:
+        if alarm[2] == "Blue":
+            count += 1
+    return count
+
+print(countAlarms(alarms))
+
