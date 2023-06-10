cd0cc23d96c82a355e7f298c18a16d2b469a2e6e
connorGA <ronnoccap@gmail.com>
2023-06-09 20:38:55 -0700

    playing with classes in python

--- interview.py
+++ interview.py
@@ -1,18 +1,29 @@
-alarms = [{"Alarm 1", "Alarmo", "Red"},
- {"Alarm 2", "Alarma", "Blue"},
- {"Alarm 3", "Alarmee", "Blue"},
- {"Alarm 4", "Alarmy", "Red"},
- {"Alarm 5", "Alarmish", "Blue"},]
+# alarms = [{"Alarm 1", "Alarmo", "Red"},
+#  {"Alarm 2", "Alarma", "Blue"},
+#  {"Alarm 3", "Alarmee", "Blue"},
+#  {"Alarm 4", "Alarmy", "Red"},
+#  {"Alarm 5", "Alarmish", "Blue"},]
 
-# Write a function that iterates through the data and counts the alarms that are Blue
+# # Write a function that iterates through the data and counts the alarms that are Blue
 
-def countAlarms(alarms):
-    count = 0
+# def countAlarms(alarms):
+#     count = 0
 
-    for alarm in alarms:
-        if alarm[2] == "Blue":
-            count += 1
-    return count
+#     for alarm in alarms:
+#         if alarm[2] == "Blue":
+#             count += 1
+#     return count
 
-print(countAlarms(alarms))
+# print(countAlarms(alarms))
+
+
+def hi():
+    print("Hello")
+
+class Person:
+    hi()
+
+    self.height: 60
+
+print(Person.height)
 
